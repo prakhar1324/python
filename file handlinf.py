8f9c5548f8278eccf4ddfa0e22f9d98a591477ea
Prakhar <prakhar.singh_cs21@gla.ac.in>
2022-05-25 21:21:13 +0530

    okss

---
+++ file handlinf.py
@@ -0,0 +1,3 @@
+file = open('Alanwalker.txt', 'r')
+for each in file:
+    print (each)
---
+++ removing.py
@@ -0,0 +1,4 @@
+prime_numbers = [2, 3, 5, 7, 9, 11]
+prime_numbers.remove(9)
+print('Updated List: ', prime_numbers)
+
