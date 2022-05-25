0792344db0dabc93ec2a541c863002bce417a548
Prakhar <prakhar.singh_cs21@gla.ac.in>
2022-05-25 19:17:20 +0530

    ok

--- abc.py
+++ abc.py
@@ -1,12 +1,10 @@
-from asyncore import read
-g= open("abc.txt","r")
-s = g.read()
-k = 0
-lines = s.count('\n')
-words = s.count(' ')
-for i in s: 
-    k = k+1
-print('sentences in file are',lines+1)
-print('words in file are',lines + words + 1)
-print('words in file are',k - lines)
-g.close()
+n = int(input("Enter number of students: "))
+d = {}
+for i in range(1,n+1):
+    name = input(f"Enter name of student with roll no. {i}: ")
+    marks = int(input(f"Enter marks of student with roll no. {i}: "))
+    d.update({i:{'Name':name,'Marks':marks}})
+sum = 0
+for i in range(1,n+1):
+    sum = sum + d[i]['Marks']
+print(sum)
---
+++ setintersection.py
@@ -0,0 +1,13 @@
+
+# eng = set()
+# fre = set()
+# n = int(input())
+# for i in map(int,input().split(' ')):
+#   eng.add(i)
+# m = int(input())
+# for i in map(int,input().split(' ')):
+#   fre.add(i)
+# sol = eng.intersection(fre)
+# print (len(sol))
+x,y =  input() ,int(input())
+print(x,y)
